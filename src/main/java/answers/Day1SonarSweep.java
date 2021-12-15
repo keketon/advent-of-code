f08c6c66f2d5bfdfbb1691141d51ff5eceb5bbb7
keketon <mas08rin10yabko@gmail.com>
2021-12-15 23:38:42 +0900

    day1 part2

--- src/main/java/answers/Day1SonarSweep.java
+++ src/main/java/answers/Day1SonarSweep.java
@@ -8,6 +8,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 @Problem(no = 1)
 public class Day1SonarSweep {
@@ -25,6 +27,34 @@ public class Day1SonarSweep {
             int now = Integer.parseInt(str);
             if (now > prv) increased++;
             prv = now;
+        }
+
+        return String.valueOf(increased);
+    }
+
+    /**
+     * No need to possess sum of window.
+     * What we only need id to compare a newcomer and a graduate of window.
+     */
+    @Part(no = 2)
+    public String increasedCountWithRange() throws IOException {
+        String resourcePath = "Day1SonarSweepInput.txt";
+        File f = new File(Const.INPUT_DIR + resourcePath);
+        BufferedReader br = new BufferedReader(new FileReader(f));
+
+        int range = 3;
+        String str;
+        int increased = 0;
+        Queue<Integer> que = new ArrayDeque<>();
+
+        for (int i = 0; i < range; i++) {
+            que.add(Integer.parseInt(br.readLine()));
+        }
+
+        while ((str = br.readLine()) != null) {
+            int now = Integer.parseInt(str);
+            if (now > que.poll()) increased++;
+            que.offer(now);
         }
 
         return String.valueOf(increased);
