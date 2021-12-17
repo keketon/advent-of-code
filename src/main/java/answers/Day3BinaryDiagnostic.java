fb0cac4d9b8002e2297408cede8e488d3fc4f72d
keketon <mas08rin10yabko@gmail.com>
2021-12-18 00:12:25 +0900

    day3 part2

--- src/main/java/answers/Day3BinaryDiagnostic.java
+++ src/main/java/answers/Day3BinaryDiagnostic.java
@@ -5,6 +5,8 @@ import annotation.Problem;
 import consts.Const;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
 @Problem(no = 3)
 public class Day3BinaryDiagnostic {
@@ -50,7 +52,69 @@ public class Day3BinaryDiagnostic {
     }
 
     @Part(no = 2)
-    public String xxx() {
-        return "";
+    public String lifeSupportRating() throws IOException {
+        init();
+
+        List<String> co2 = new LinkedList<>();
+        List<String> o2 = new LinkedList<>();
+        String str;
+        while ((str = br.readLine()) != null) {
+            co2.add(str);
+            o2.add(str);
+        }
+
+        int n = co2.get(0).length();
+
+        for (int i = 0; i < n; i++) {
+            List<String> bitStands = new LinkedList<>();
+            List<String> bitNotStands = new LinkedList<>();
+
+            int sum = 0;
+
+            for (String bit : co2) {
+                if (bit.charAt(i) == '1') {
+                    sum++;
+                    bitStands.add(bit);
+                } else {
+                    bitNotStands.add(bit);
+                }
+            }
+
+            co2 = 2 * sum >= co2.size() ? bitStands : bitNotStands;
+            if (co2.size() == 1) break;
+        }
+
+        for (int i = 0; i < n; i++) {
+            List<String> bitStands = new LinkedList<>();
+            List<String> bitNotStands = new LinkedList<>();
+
+            int sum = 0;
+
+            for (String bit : o2) {
+                if (bit.charAt(i) == '1') {
+                    sum++;
+                    bitStands.add(bit);
+                } else {
+                    bitNotStands.add(bit);
+                }
+            }
+
+            o2 = 2 * sum < o2.size() ? bitStands : bitNotStands;
+            if (o2.size() == 1) break;
+        }
+
+        int co2Res = 0, o2Res = 0;
+
+        for (int i = 0; i < n; i++) {
+            co2Res *= 2;
+            o2Res *= 2;
+
+            co2Res += co2.get(0).charAt(i) - '0';
+            o2Res += o2.get(0).charAt(i) - '0';
+        }
+
+        int ans = co2Res * o2Res;
+
+        return String.valueOf(ans);
     }
 }
