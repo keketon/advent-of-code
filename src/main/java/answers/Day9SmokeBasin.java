51b65d0e512170b9d8ab8617805a402b44a0122a
keketon <mas08rin10yabko@gmail.com>
2021-12-30 20:53:55 +0900

    day9 part2

--- src/main/java/answers/Day9SmokeBasin.java
+++ src/main/java/answers/Day9SmokeBasin.java
@@ -5,9 +5,7 @@ import annotation.Problem;
 import consts.Const;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Problem(no = 9)
@@ -56,5 +54,69 @@ public class Day9SmokeBasin {
         }
 
         return String.valueOf(ans);
+    }
+
+    @Part(no = 2)
+    public String islandNum() throws IOException {
+        init();
+
+        int wall = 9;
+        int[] dr = {1, 0, -1, 0};
+        int[] dc = {0, 1, 0, -1};
+
+        List<List<Integer>> caveHeight = new ArrayList<>();
+        String cursor = br.readLine();
+        while (cursor != null) {
+            caveHeight.add(Arrays.stream(cursor.split("")).map(Integer::parseInt).collect(Collectors.toList()));
+            cursor = br.readLine();
+        }
+
+        int r = caveHeight.size();
+        int c = caveHeight.get(0).size();
+
+        List<Integer> islandNums = new ArrayList<>();
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (caveHeight.get(i).get(j) == wall) continue;
+
+                int size = 0;
+                Queue<Coordinate> que = new ArrayDeque<>();
+                que.add(new Coordinate(i, j));
+
+                while (!que.isEmpty()) {
+                    Coordinate coordinate = que.poll();
+                    if (caveHeight.get(coordinate.r).get(coordinate.c) == wall) continue;
+
+                    for (int k = 0; k < dr.length; k++) {
+                        int nr = coordinate.r + dr[k];
+                        int nc = coordinate.c + dc[k];
+
+                        if (nr >= 0 && nr < r && nc >= 0 && nc < c && caveHeight.get(nr).get(nc) != wall) {
+                            que.add(new Coordinate(nr, nc));
+                        }
+                    }
+
+                    caveHeight.get(coordinate.r).set(coordinate.c, wall);
+                    size++;
+                }
+
+                islandNums.add(size);
+            }
+        }
+
+        int ans = islandNums.stream().sorted(Comparator.reverseOrder()).limit(3).reduce((acc, now) -> acc * now).get();
+
+        return String.valueOf(ans);
+    }
+
+    private static class Coordinate {
+        int r;
+        int c;
+
+        public Coordinate(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
     }
 }
